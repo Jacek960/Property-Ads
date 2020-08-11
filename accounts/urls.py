7fea7136b60eeb5534e37e7cec1c137b275e20bf
Jacek K <szachy960@gmail.com>
2020-08-11 18:51:28 +0200

    profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, UserProfile, UserUpdate
 
 urlpatterns = [
 path('signup/', SignUpView.as_view(), name='signup'),
+path('profil/', UserProfile.as_view(), name='user_profile'),
+path('user_update_profile/', UserUpdate.as_view(), name='user-update-profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views import View
+from django.views.generic import UpdateView
+
+from accounts.forms import UserUpdateForm
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class UserProfile(LoginRequiredMixin, View):
+    def get(self, request):
+        user = User.objects.get(username=request.user)
+        return render(request, 'accounts/user_profile.html', {'user': user})
+
+class UserUpdate(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = 'accounts/user_update_form.html'
+    success_url = '/accounts/profil/'
+
+    def get_object(self, queryset=None):
+        return self.request.user
